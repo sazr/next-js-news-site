f4966c7d0655a52fb80d9f7ea3219333e6f0b636
sazr <samzielkeryner@gmail.com>
2023-11-19 11:25:12 +1000

    Add testing suite

---
+++ cypress/e2e/createSurvey.cy.js
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker';
+
+describe('Create Survey Page', () => {
+  it('should redirect to login page when user is not logged in', () => {
+    cy.visit('/survey/create');
+    cy.url().should('include', '/login');
+  });
+
+  it('should create a survey', () => {
+    const surveyTitle = faker.lorem.sentence();
+    const questionContent = faker.lorem.sentence();
+
+    cy.login();
+    cy.get('[data-test-id="create-survey"]').click();
+    cy.url().should('include', '/survey/create');
+    cy.get('input[name="survey-title"]').type(surveyTitle);
+
+    cy.get('input[data-test-id="question-input-0"]').type(questionContent);
+    cy.get('input[data-test-id="question-input-1"]').type(questionContent);
+
+    cy.get('button[name="create-survey"]').click();
+
+    console.log('url', cy.url());
+    cy.url().should('include', '/survey/answer/');
+    cy.visit('/surveys');
+    cy.contains(surveyTitle);
+  });
+});
